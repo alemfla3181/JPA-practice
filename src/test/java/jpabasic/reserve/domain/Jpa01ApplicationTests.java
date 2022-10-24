6fe726d0bdd64048cab5be845781439ae3e2b5cc
dragon <alemfla3181@naver.com>
2022-10-24 22:27:02 +0900

    JPA study

---
+++ src/test/java/jpabasic/reserve/domain/Jpa01ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.reserve.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
